4fabe94230c76e6bfa26b27e60edc5910464a1ef
dong <2990978298@qq.com>
2022-04-08 17:27:10 +0800

    添加scheduler

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,13 +1,13 @@
 class ReactiveEffect {
   private _fn : any
 
-  constructor(fn) {
+  constructor(fn, public scheduler?) {
     this._fn = fn
   }
 
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
 }
 
@@ -36,12 +36,18 @@ export function trigger(target,key) {
   let dep = depsMap.get(key)
 
   for (let effect of dep) {
-    effect.run()
+    if(effect.scheduler){
+      effect.scheduler()
+    } else{
+      effect.run()
+    }
   }
 }
 
 let activeEffect
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn)
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler)
   _effect.run()
+
+  return _effect.run.bind(_effect)
 }
