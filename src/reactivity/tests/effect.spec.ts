a0efb7ed15b9ada872a6be43300bc75575017a9b
dong <2990978298@qq.com>
2022-04-08 17:26:52 +0800

    添加scheduler单元测试

--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -19,4 +19,41 @@ describe("effect", () => {
     expect(nextAge).toBe(12)
     
   })
+
+  // effect -> function(runner) -> fn -> return
+  it("should return runner when call effect", ()=>{
+    let foo = 10
+    const runner = effect(() => {
+      foo++
+      return "foo"
+    })
+
+    expect(foo).toBe(11)
+    const r = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe("foo")
+
+  })
+
+  it("scheduler", () => {
+    let dummy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dummy = obj.foo;
+      },
+      { scheduler }
+    );
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1);
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    expect(dummy).toBe(1)
+    run()
+    expect(dummy).toBe(2)
+  })
 })
