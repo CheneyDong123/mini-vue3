39e8b35c8f573ab60cd4448129a19181f0001275
dong <2990978298@qq.com>
2022-04-09 18:49:54 +0800

    添加stop与onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,16 +1,37 @@
+import { extend } from "../shared"
+
 class ReactiveEffect {
   private _fn : any
-
-  constructor(fn, public scheduler?) {
+  active = true
+  deps = []
+  onStop?: () => void
+  public scheduler: Function | undefined
+  constructor(fn, scheduler?: Function) {
     this._fn = fn
+    this.scheduler = scheduler
   }
 
   run() {
     activeEffect = this
     return this._fn()
   }
+
+  stop() {
+    if(this.active){
+      clearupEffect(this)
+      if(this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
 }
 
+function clearupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  });
+}
 
 // target -> key -> dep(fn)
 const targetsMap = new Map()
@@ -28,7 +49,10 @@ export function track(target,key) {
     depsMap.set(key,dep)
   }
 
+
+  if(!activeEffect) return
   dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
 export function trigger(target,key) {
@@ -38,16 +62,29 @@ export function trigger(target,key) {
   for (let effect of dep) {
     if(effect.scheduler){
       effect.scheduler()
-    } else{
+    } else {
       effect.run()
     }
   }
 }
 
+
 let activeEffect
+
 export function effect(fn, options: any = {}) {
+
   const _effect = new ReactiveEffect(fn, options.scheduler)
+
+  extend(_effect, options)
+
   _effect.run()
 
-  return _effect.run.bind(_effect)
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+
+  return runner;
+}
+
+export function stop(runner) {
+   runner.effect.stop()
 }
