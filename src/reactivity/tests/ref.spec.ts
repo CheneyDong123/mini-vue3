2b7a5d620da57e6c463d02ba59477a76834effc9
dong <2990978298@qq.com>
2022-04-14 19:19:11 +0800

    添加proxyRef单元测试

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from "../effect"
 import { reactive } from "../reative"
-import { isRef, ref, unref } from "../ref"
+import { isRef, proxyRefs, ref, unref } from "../ref"
 
 describe("ref", () => {
   it("happy parh", () => {
@@ -42,7 +42,7 @@ describe("ref", () => {
 
   it("isRef", () => {
     const a = ref(1)
-    const user  = reactive({
+    const user = reactive({
       age: 1
     })
     expect(isRef(a)).toBe(true)
@@ -52,8 +52,28 @@ describe("ref", () => {
 
   it("unref", () => {
     const a = ref(1)
-    
+
     expect(unref(a)).toBe(1)
     expect(unref(1)).toBe(1)
+  })
+
+  it("proxyRefs", () => {
+    const user = {
+      age: ref(10),
+      name: "xiaoHong"
+    }
+
+    const proxyUser = proxyRefs(user)
+    expect(user.age.value).toBe(10)
+    expect(proxyUser.age).toBe(10)
+    expect(proxyUser.name).toBe("xiaoHong")
+
+    proxyUser.age = 20
+    expect(proxyUser.age).toBe(20)
+    expect(user.age.value).toBe(20)
+
+    proxyUser.age = ref(10)
+    expect(proxyUser.age).toBe(10)
+    expect(user.age.value).toBe(10)
   })
 })
